98720d62a66e58d422e799284a9694126ac6337a
Steve Wilburn <stevewilburn@Steves-MacBook-Air.local>
2024-08-16 18:10:53 -0700

    lsp and trouble

--- lua/swilburn/lazy.lua
+++ lua/swilburn/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("swilburn.plugins", {
+require("lazy").setup({ { import = "swilburn.plugins" }, { import = "swilburn.plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,
