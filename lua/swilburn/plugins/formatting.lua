f887a19651feacd6ee2b3562c771a369eb9db94e
Steve <tunafishman@gmail.com>
2024-08-16 19:07:34 -0700

    I think I'm done
    this takes me all the way through Josean's youtube tutorial for nvim
    setup

---
+++ lua/swilburn/plugins/formatting.lua
@@ -0,0 +1,31 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        javascripts = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        python = { "isort", "black" },
+        lua = { "stylua" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      },
+    })
+
+    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      })
+    end, { desc = "Format file or range (in visual mode)" })
+  end,
+}
--- lua/swilburn/plugins/lsp/mason.lua
+++ lua/swilburn/plugins/lsp/mason.lua
@@ -1,7 +1,8 @@
-return{
+return {
   "williamboman/mason.nvim",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
     -- import mason
@@ -9,6 +10,8 @@ return{
 
     --import mason-lspconfig
     local mason_lspconfig = require("mason-lspconfig")
+
+    local mason_tool_installer = require("mason-tool-installer")
 
     mason.setup({
       ui = {
@@ -27,10 +30,19 @@ return{
         "html",
         "cssls",
         "pyright",
-        "biome"
-      }
+        "biome",
+      },
     })
 
-
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "prettier",
+        "stylua",
+        "isort",
+        "black",
+        "pylint",
+        "eslint_d",
+      },
+    })
   end,
 }
